498499b91196ee5dcda19ba56e7f8628ca53c12b
Rohan Patel <rohandvp@gmail.com>
2025-12-30 16:16:28 -0500

    api is working

--- api.py
+++ api.py
@@ -1,51 +1,67 @@
 from fastapi import FastAPI
-import datetime
+from datetime import date
 from supabase import create_client, Client
 import psycopg2
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+
+load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
+client = None
+
 try:
     client = create_client(url, key)
     print("Connection Success")
-except Exception e:
-    print(e.stackTrace())
+except Exception as e:
+    print(e)
 
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
 
 @app.get("/date-movement/{date}")
-async def getDateMovement(date: Date):
-    response = client.table("finra_metrics")
-        .select("*")
-        .eq("finra_metrics.date", date)
-        .execute()
+async def getDateMovement(date: date):
+    response = client.table("finra_metrics") \
+        .select("*") \
+        .eq("date", date) \
+        .execute() 
     return response
 
 @app.get("sector-movement/{sector}")
 async def getSectorMovement(sector: str):
-    response = client.table("finra_metrics")
-        .select("*")
-        .eq("finra_metrics.sector", sector)
+    response = client.table("finra_metrics") \
+        .select("*") \
+        .eq("sector", sector) \
         .execute()
     return response
 
 @app.get("industry-movement/{industry}")
 async def getIndustryMovement(industry: str):
-    response = client.table("finra_metrics")
-        .select("*")
-        .eq("finra_metrics.industry", industry)
+    response = client.table("finra_metrics") \
+        .select("*") \
+        .eq("industry", industry) \
         .execute()
     return response
 
 @app.get("symbol-movement/{symbol}")
 async def getSymbolMovement(symbol: str):
-    response = client.table("finra_metrics")
-        .select("*")
-        .eq("finra_metrics.symbol", symbol)
+    response = client.table("finra_metrics") \
+        .select("*") \
+        .eq("symbol", symbol) \
         .execute()
     return response
+
+
+def getSymbolMovementTest(symbol: str):
+    response = client.table("finra_metrics") \
+        .select("*") \
+        .eq("symbol", symbol) \
+        .execute()
+    return response
+
+print(getSymbolMovementTest("AAPL"))
